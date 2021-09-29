d867c33d6728d63c739deaac448bfc7393e0636d
Bsir-Rachida <90473644+Bsir-Rachida@users.noreply.github.com>
2021-09-29 09:32:56 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+$characters = [
+
+    "Negan" => [
+
+        "city" => "The Sanctuary",
+
+        "weapon" => "Lucille"
+
+    ],
+
+    "Daryl" => [
+
+        "city" => "Alexandria",
+
+        "weapon" => "crossbow"
+
+    ],
+
+    "Ezekiel" => [
+
+        "city" => "The Kingdom",
+
+        "weapon" => "Shiva"
+
+    ]
+
+];
+
+$characters['bsir rachida'] = ['Orléans','pistolet'];
+var_dump ($characters);
+
